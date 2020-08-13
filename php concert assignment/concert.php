317dbd46fab80dcfd31e074c6e9b1c16992e09c1
notynote <epochmaking.th@gmail.com>
2020-08-13 15:02:53 +0700

    finish concert assignment

--- php concert assignment/concert.php
+++ php concert assignment/concert.php
@@ -4,6 +4,8 @@ $seats = array(
     "A1","A2","A3","B1","B2","B3","C1","C2","C3"
 );
 
+$noSeat = 0;
+
 ?>
 
 <!DOCTYPE html>
@@ -20,75 +22,41 @@ $seats = array(
     <div class="container">
         <form method="POST" action="concertResult.php">
             <div class="form-group">
-                <div class="row">
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="A1" id="A1" name="A1">
-                        <label class="form-check-label" for="A1">
-                            A1
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="A2" id="A2" name="A2">
-                        <label class="form-check-label" for="A2">
-                            A2
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="A3" id="A3" name="A3">
-                        <label class="form-check-label" for="A3">
-                            A3
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="B1" id="B1" name="B1">
-                        <label class="form-check-label" for="B1">
-                            B1
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="B2" id="B2" name="B2">
-                        <label class="form-check-label" for="B2">
-                            B2
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="B3" id="B3" name="B3">
-                        <label class="form-check-label" for="B3">
-                            B3
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="C1" id="C1" name="C1">
-                        <label class="form-check-label" for="C1">
-                            C1
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="C2" id="C2" name="C2">
-                        <label class="form-check-label" for="C2">
-                            C2
-                        </label>
-                    </div>
-                    <div class="form-check mx-3">
-                        <input class="form-check-input" type="checkbox" value="C3" id="C3" name="C3">
-                        <label class="form-check-label" for="C3">
-                            C3
-                        </label>
-                    </div>
-                </div>
-                <div class="row my-3">
-                    <button type="submit" class="btn btn-primary">Buy now!</button>
+                <h3 class="my-4 text-center">Private Concert<p class="lead">Choose your seats <span class="text-danger font-italic">(Maximum 4 tickets)</p></h3>
+                
+                <?php
+
+                    foreach($seats as $seat) {
+
+                        if ($noSeat%3 === 0){
+                            echo "<div class='row my-3'>";
+                        }
+
+                        echo "
+                        <div class='col text-center'>
+                        <div class='form-check'>
+                            <input style='width: 20px; height: 20px;' class='form-check-input' type='checkbox' value=".$seat." id=".$seat." name=".$seat.">
+                            <label class='form-check-label ml-2 h6' for=".$seat.">".$seat."</label>
+                        </div></div>";
+
+                        if ($noSeat%3 === 2){
+                            echo "</div>";
+                        }
+
+                        $noSeat++;
+                        
+                    }
+
+                ?>
+                <div class="row my-4">
+                    <button type="submit" class="btn btn-primary mx-auto">Buy now!</button>
                 </div>
             </div>
         </form>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
--- php concert assignment/concertResult.php
+++ php concert assignment/concertResult.php
@@ -15,25 +15,6 @@ $seatsPrice = array(
 $totalPrice = 0;
 $noSeat = 0;
 
-foreach($seatsPrice as $seat => $price) {
-    
-
-    if (isset($_POST[$seat])){
-        echo $seat.": ".$price."<br>";
-        $totalPrice += $price;
-        $noSeat++;
-    }
-   
-}
-
-if ($noSeat === 0) {
-    echo "<span style='color:#FF0000;'>Please select some ticket</span>";
-} else {
-    echo "Number of seats: ".$noSeat."<br>Total Price: ".$totalPrice;
-}
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -41,9 +22,61 @@ if ($noSeat === 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Concert Result by Pathompong Phongsaporamut</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style> body,html { height:100%; } </style>
 </head>
 <body>
+
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="jumbotron text-center">
+                <h1>Tickets Receipt</h1>
+                <hr class="my-2">
+                <p>
+                <?php
+                    foreach($seatsPrice as $seat => $price) {
     
+                        if (isset($_POST[$seat])){
+
+                            if ($seat === 'A3') {
+
+                                $luckyPrice = $price/2;
+
+                                echo $seat.": ".$luckyPrice." THB <span class='text-danger font-italic'>*lucky draw 50% off*</span><br>";
+                                $totalPrice += $luckyPrice;
+
+                            } else {
+                                echo $seat.": ".$price." THB<br>";
+                                $totalPrice += $price;
+                            }
+
+                            $noSeat++;
+                        }
+                       
+                    }
+
+                    if ($noSeat === 0) {
+                        echo "<span style='color:#FF0000;'>Please select some ticket</span>";
+                        echo "<script type='text/javascript'>alert('Please select some ticket!'); window.location.href='concert.php';</script>";
+                    
+                    } elseif ($noSeat > 4) {
+                        echo "<script type='text/javascript'>alert('Maximum 4 Seats!'); window.location.href='concert.php';</script>";
+                    
+                    } else {
+                        echo "<br>Number of seats: <span class='font-weight-bold'>".$noSeat."</span><br>Total Price: <span class='font-weight-bold'>".$totalPrice." THB</span>";
+                    }
+                ?>
+                </p>
+                <p class="lead">
+                    <a class="btn btn-info" href="concert.php" role="button"><< Back</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
